23b386bafcdc12a91f05f20709379403671ec37b
A. T. Campbell, III <atcampbelliii@gmail.com>
2018-08-08 11:27:40 -0500

    Rename Hello project. Add Variabl;es project.

---
+++ 02Variables/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Task 1
+let name = "John";
+let admin = name;
+alert(admin);
+
+// Task 2
+let planetName = "Earth";
+let visitor = "Mork";
+let message = visitor + " is visiting " + planetName;
+alert(message);
