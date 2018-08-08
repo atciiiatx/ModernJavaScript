7b8454ed6e5be019290f8dd916279a00fff2fb2b
A. T. Campbell, III <atcampbelliii@gmail.com>
2018-08-08 11:51:16 -0500

    Add DataTypes project

---
+++ 03DataTypes/script.js
@@ -0,0 +1,25 @@
+"use strict";
+let str = "John";
+let phrase = `Hello, ${str}`
+alert(phrase);
+
+let isGreater = 4 > 1;
+alert(isGreater);
+
+let age = null;
+alert(age);
+
+let x;
+alert(x);
+
+alert(typeof str);
+alert(typeof isGreater);
+alert(typeof age);
+alert(typeof x);
+alert(typeof alert);
+
+// Tasl
+let name = "Ilya";
+alert( `hello ${1}` ); // hello 1
+alert( `hello ${"name"}` ); //  hello name
+alert( `hello ${name}` ); // hello Ilya
